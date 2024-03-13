71d836c95f81cd494aa374988d9a211221029dcf
Zac <zachary.sanson@taitcommunication.com>
2024-03-13 14:51:09 +1300

    Service bus exercise

---
+++ exercise-service-bus/performancemessagereceiver/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace performancemessagereceiver
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string TopicName = "salesperformancemessages";
+        const string SubscriptionName = "Americas";
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            // Create a Service Bus client that will authenticate using a connection string
+            var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after receiving all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Create the options to use for configuring the processor
+            var processorOptions = new ServiceBusProcessorOptions
+            {
+                MaxConcurrentCalls = 1,
+                AutoCompleteMessages = false
+            };
+            // Create a processor that we can use to process the messages
+            ServiceBusProcessor processor = client.CreateProcessor(TopicName, SubscriptionName, processorOptions);
+            // Configure the message and error handler to use
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+            // Start processing
+            await processor.StartProcessingAsync();
+            Console.Read();
+
+            // Since we didn't use the "await using" syntax here, we need to explicitly dispose the processor and client
+            await processor.DisposeAsync();
+            await client.DisposeAsync();
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{args.Message.SequenceNumber} Body:{args.Message.Body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine($"Message handler encountered an exception {args.Exception}.");
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {args.FullyQualifiedNamespace}");
+            Console.WriteLine($"- Entity Path: {args.EntityPath}");
+            Console.WriteLine($"- Executing Action: {args.ErrorSource}");
+            return Task.CompletedTask;
+        }
+    }
+}
